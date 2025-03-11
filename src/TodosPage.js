4fc929ee71beae19e6acdaf342663bb693cd0775
Sandeep <kotakondlasandeep2323@gmail.com>
2025-03-11 18:07:03 +0530

    CalendarData - calendar Data has been calling from the API

--- src/TodosPage.js
+++ src/TodosPage.js
@@ -10,9 +10,9 @@ const TodosPage = ({sampleData}) =>{
         <div className="list"> 
             <h2>{currentDate.date()} {currentDate.format("MMMM")}, {currentDate.format("YYYY")}</h2>
             {sampleData.map((prev,index) => prev.month === currentDate.format("MMMM") &&
-            prev.year === parseInt(currentDate.format("YYYY")) &&
-            prev[currentDate.date()] ? 
-            prev[currentDate.date()].events.map(item => item.from >= currentDate.hour() ?  
+            parseInt(prev.year) === parseInt(currentDate.format("YYYY")) &&
+            parseInt(prev.date) === currentDate.date() ? 
+            prev.events.map(item => item.from >= currentDate.hour() ?  
             <div key={index} className="list-item" style={{boxShadow:`0px 2px 5px ${colors[index % colors.length]}`}}>
                 <span>{dayjs().hour(item.from).format("h A")} - {dayjs().hour(item.to).format("h A")}</span>
                 <span>{item.title}</span>
