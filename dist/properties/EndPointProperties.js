742fe9ad09435ebe6e0bd63209ccd1546bcdf5cb
narender <snr@trunksoft.in>
2025-06-04 16:59:41 -0500

    Updating properties

--- dist/properties/EndPointProperties.js
+++ dist/properties/EndPointProperties.js
@@ -1,6 +1,7 @@
 export const MEMBERS_API = "https://kh9zku31eb.execute-api.us-east-1.amazonaws.com/dev/users/";
 export const RESOURCES_API = "https://bws4su8xog.execute-api.us-east-1.amazonaws.com/dev/resources/";
-export const CALENDAR_API = "https://pliol7eyw7.execute-api.us-east-1.amazonaws.com/dev/calendar/";
+export const CALENDAR_API = "https://pliol7eyw7.execute-api.us-east-1.amazonaws.com/dev/calendar/user/";
+export const RECURRING_CALENDAR_API = "https://pliol7eyw7.execute-api.us-east-1.amazonaws.com/dev/";
 export const DASHBOARD_API = "https://n82x1bmdef.execute-api.us-east-1.amazonaws.com/dev/dashboard/";
 export const CATEGORIES_API = "https://cx8712tt7e.execute-api.us-east-1.amazonaws.com/dev/categories/";
 export const ITEMS_API = "https://1p6bhnsms5.execute-api.us-east-1.amazonaws.com/dev/items/";
